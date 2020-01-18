39da997e7908622b8769b1a03c43697e9c864de3
KaKa <kaka@kakawebsitedemo.com>
2020-01-18 14:43:44 +0800

    Wait Until domcontentloaded

--- cron/getRevenue.js
+++ cron/getRevenue.js
@@ -5,11 +5,11 @@ const path = require('path');
 const URL = "http://game-i.daa.jp/?%E3%82%A2%E3%83%97%E3%83%AA%2F%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89%E3%83%95%E3%83%AA%E3%83%83%E3%83%91%E3%83%BC%28WORLD%20FLIPPER%29";
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--single-process'] });
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--single-process'], headless: true });
   const page = await browser.newPage();
 
   try {
-    await page.goto(URL);
+    await page.goto(URL, {waitUntil: 'domcontentloaded'});
 
     const svg = await page.$('svg');
 
