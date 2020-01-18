92b0dfc35e4e09479e2141b5b797c9b0e814e730
KaKa <kaka@kakawebsitedemo.com>
2020-01-18 14:54:04 +0800

    Update Cron

--- cron/getRevenue.js
+++ cron/getRevenue.js
@@ -1,25 +1,27 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 
-
-const URL = "http://game-i.daa.jp/?%E3%82%A2%E3%83%97%E3%83%AA%2F%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89%E3%83%95%E3%83%AA%E3%83%83%E3%83%91%E3%83%BC%28WORLD%20FLIPPER%29";
+const URL =
+  'http://game-i.daa.jp/?%E3%82%A2%E3%83%97%E3%83%AA%2F%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89%E3%83%95%E3%83%AA%E3%83%83%E3%83%91%E3%83%BC%28WORLD%20FLIPPER%29';
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--single-process'], headless: true });
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process'],
+    headless: true
+  });
   const page = await browser.newPage();
 
   try {
-    await page.goto(URL, {waitUntil: 'domcontentloaded'});
+    await page.goto(URL, { waitUntil: ['load', 'networkidle0'] });
 
     const svg = await page.$('svg');
 
-    !!svg && await svg.screenshot({ path: path.join(__dirname, 'revenue.png') });
+    !!svg && (await svg.screenshot({ path: path.join(__dirname, 'revenue.png') }));
 
     const table = await page.$$('.style_table');
     const rank = await table[1].$('tbody');
 
-    !!rank && await rank.screenshot({ path: path.join(__dirname, 'revenue_rank.png') });
-
+    !!rank && (await rank.screenshot({ path: path.join(__dirname, 'revenue_rank.png') }));
   } catch (e) {
     console.error(e);
   } finally {
