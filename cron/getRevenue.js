a56fa91a4ad4affbfdf3617130ca58fe96cbc51f
KaKa <kaka@kakawebsitedemo.com>
2020-01-18 14:34:45 +0800

    Fix: Puppeteer

--- cron/getRevenue.js
+++ cron/getRevenue.js
@@ -5,11 +5,11 @@ const path = require('path');
 const URL = "http://game-i.daa.jp/?%E3%82%A2%E3%83%97%E3%83%AA%2F%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89%E3%83%95%E3%83%AA%E3%83%83%E3%83%91%E3%83%BC%28WORLD%20FLIPPER%29";
 
 (async () => {
-  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--single-process'] });
   const page = await browser.newPage();
 
   try {
-    await page.goto(URL, { waitUntil: 'networkidle0' });
+    await page.goto(URL);
 
     const svg = await page.$('svg');
 
