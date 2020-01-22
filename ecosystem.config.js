6bf70de12a74014d0877f4be23a6bebf210d460e
Barcode <barcode0108@gmail.com>
2020-01-22 23:31:33 +0800

    Should restart when index change

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,6 +5,6 @@ module.exports = {
     args: 'index.ts',
     autorestart: true,
     source_map_support: true,
-    watch: ['commands/**/*','data/**/*','utilities/**/*', 'cron/**/*', '.env']
+    watch: ['index.ts', 'commands/**/*','data/**/*','utilities/**/*', 'cron/**/*', '.env']
   }]
 }
