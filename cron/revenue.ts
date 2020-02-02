773866544d5a77cc8292feb44337c6f833220722
Barcode <barcode0108@gmail.com>
2020-02-03 06:51:52 +0800

    update revenue

--- cron/revenue.ts
+++ cron/revenue.ts
@@ -2,8 +2,11 @@ import { CronJob } from 'cron';
 import * as puppeteer from 'puppeteer';
 import * as path from 'path';
 
-const URL =
+const WF_URL =
   'http://game-i.daa.jp/?%E3%82%A2%E3%83%97%E3%83%AA%2F%E3%83%AF%E3%83%BC%E3%83%AB%E3%83%89%E3%83%95%E3%83%AA%E3%83%83%E3%83%91%E3%83%BC%28WORLD%20FLIPPER%29';
+
+const GBF_URL =
+  'http://game-i.daa.jp/?%E3%82%A2%E3%83%97%E3%83%AA%2F%E3%82%B0%E3%83%A9%E3%83%B3%E3%83%96%E3%83%AB%E3%83%BC%E3%83%95%E3%82%A1%E3%83%B3%E3%82%BF%E3%82%B8%E3%83%BC';
 
 export async function revenueFunction() {
   const browser = await puppeteer.launch({
@@ -11,17 +14,22 @@ export async function revenueFunction() {
   });
   const page = await browser.newPage();
 
-  try {
-    await page.goto(URL, { waitUntil: ['load', 'domcontentloaded'], timeout: 0 });
+  async function getShot(url: string, prefix: string = '') {
+    await page.goto(url, { waitUntil: ['load', 'domcontentloaded'], timeout: 0 });
 
     const svg = await page.$('svg');
 
-    await svg?.screenshot({ path: path.join(__dirname, '..', 'assets', 'revenue.png') });
+    await svg?.screenshot({ path: path.join(__dirname, '..', 'assets', `${prefix}revenue.png`) });
 
     const table = await page.$$('.style_table');
     const rank = await table[1].$('tbody');
 
-    await rank?.screenshot({ path: path.join(__dirname, '..', 'assets', 'revenue_rank.png') });
+    await rank?.screenshot({ path: path.join(__dirname, '..', 'assets', `${prefix}revenue_rank.png`) });
+  }
+
+  try {
+    await getShot(WF_URL);
+    await getShot(GBF_URL, 'gbf_');
   } catch (e) {
     console.error(e);
   } finally {
